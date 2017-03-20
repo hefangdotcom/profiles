164a69784bf227390464c5a556066d34ef0f8b16
hefangdotcom <hefangdotcom@gmail.com>
2017-03-20 06:58:57 -0400

    chibuild update

--- chibuild/recon.sh
+++ chibuild/recon.sh
@@ -1,6 +1,6 @@
 #!/opt/exp/bin/bash
 cmd_out=$(ps -ax |grep 6123:localhost:22 |grep -v grep 2>&1)
-ssh_cmd="ssh -N -f -R 6123:localhost:22 google"
+ssh_cmd="/opt/exp/bin/ssh -N -f -R 6123:localhost:22 google"
 if [$cmd_out eq '']; then
 exec $ssh_cmd
 else
